17041084c1d221f83a4ede4cd1f284e241f69324
neoz666 <neoz.blockchain@gmail.com>
2024-09-21 20:03:02 +0530

    Initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
+use web3::types::{Address, U256};
+use web3::contract::{Contract, Options};
+
 #[tokio::main]
 async fn main() -> web3::Result<()> {
-    let transport = web3::transports::Http::new("http://localhost:7545")?;
-    let web3 = web3::Web3::new(transport);
+    let ws = WebSocket::new("wss://mainnet.infura.io/ws/v3/YOUR_INFURA_PROJECT_ID").await?;
+    let web3 = Web3::new(ws);
 
-    println!("Calling accounts.");
-    let mut accounts = web3.eth().accounts().await?;
-    println!("Accounts: {:?}", accounts);
-    accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
+    let contract_address: Address = "0xYourContractAddress".parse().unwrap();
+    let abi = include_bytes!("path/to/your_contract_abi.json");
+    let contract = Contract::from_json(web3.eth(), contract_address, abi)?;
 
-    println!("Calling balance.");
-    for account in accounts {
-        let balance = web3.eth().balance(account, None).await?;
-        println!("Balance of {:?}: {}", account, balance);
-    }
+    // Example: Call a read-only method
+    let result: U256 = contract.query("balanceOf", ("0xYourAddress".parse::<Address>().unwrap(),), None, Options::default(), None).await?;
+    println!("Balance: {:?}", result);
 
     Ok(())
 }
